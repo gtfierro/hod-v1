3bd3850464d22f53d68f8528f928dc77007f7c03
Gabe Fierro <gtfierro225@gmail.com>
2016-12-12 17:13:16 -0800

    add test case

--- db/db_test.go
+++ db/db_test.go
@@ -30,15 +30,20 @@ func TestDBQuery(t *testing.T) {
 			"SELECT ?x WHERE { ?ahu rdf:type brick:AHU . ?ahu bf:feeds+ ?x .};",
 			[]ResultMap{{"?x": turtle.ParseURI("http://buildsys.org/ontologies/building_example#hvaczone_1")}, {"?x": turtle.ParseURI("http://buildsys.org/ontologies/building_example#vav_1")}},
 		},
+		{
+			"SELECT ?vav ?room WHERE { ?vav rdf:type brick:VAV . ?room rdf:type brick:Room . ?zone rdf:type brick:HVAC_Zone . ?vav bf:feeds+ ?zone . ?room bf:isPartOf ?zone . }; ",
+			[]ResultMap{{"?room": turtle.ParseURI("http://buildsys.org/ontologies/building_example#room_1"), "?vav": turtle.ParseURI("http://buildsys.org/ontologies/building_example#vav_1")}},
+		},
 	} {
 		q, e := query.Parse(strings.NewReader(test.query))
 		if e != nil {
-			t.Error(e)
+			t.Error(test.query, e)
 			continue
 		}
 		results := db.RunQuery(q)
 		if !compareResultMapList(test.results, results) {
 			t.Errorf("Results for %s had\n %+v\nexpected\n %+v", test.query, results, test.results)
+			return
 		}
 	}
 }
