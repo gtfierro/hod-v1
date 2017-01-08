15f807f8442fd136d04f397823ef14c6afa36a12
Gabe Fierro <gtfierro225@gmail.com>
2017-01-08 14:52:54 -0800

    remember to include key impl

---
+++ db/key.go
@@ -0,0 +1,19 @@
+//go:generate msgp
+package db
+
+import (
+	"encoding/binary"
+
+	"github.com/google/btree"
+)
+
+type Key [4]byte
+
+func (k Key) Less(than btree.Item) bool {
+	t := than.(Key)
+	return binary.LittleEndian.Uint32(k[:]) < binary.LittleEndian.Uint32(t[:])
+}
+
+func (k *Key) FromSlice(src []byte) {
+	copy(k[:], src)
+}
---
+++ db/key_gen.go
@@ -0,0 +1,47 @@
+package db
+
+// NOTE: THIS FILE WAS PRODUCED BY THE
+// MSGP CODE GENERATION TOOL (github.com/tinylib/msgp)
+// DO NOT EDIT
+
+import "github.com/tinylib/msgp/msgp"
+
+// DecodeMsg implements msgp.Decodable
+func (z *Key) DecodeMsg(dc *msgp.Reader) (err error) {
+	err = dc.ReadExactBytes(z[:])
+	if err != nil {
+		return
+	}
+	return
+}
+
+// EncodeMsg implements msgp.Encodable
+func (z *Key) EncodeMsg(en *msgp.Writer) (err error) {
+	err = en.WriteBytes(z[:])
+	if err != nil {
+		return
+	}
+	return
+}
+
+// MarshalMsg implements msgp.Marshaler
+func (z *Key) MarshalMsg(b []byte) (o []byte, err error) {
+	o = msgp.Require(b, z.Msgsize())
+	o = msgp.AppendBytes(o, z[:])
+	return
+}
+
+// UnmarshalMsg implements msgp.Unmarshaler
+func (z *Key) UnmarshalMsg(bts []byte) (o []byte, err error) {
+	bts, err = msgp.ReadExactBytes(bts, z[:])
+	if err != nil {
+		return
+	}
+	o = bts
+	return
+}
+
+func (z *Key) Msgsize() (s int) {
+	s = msgp.ArrayHeaderSize + (4 * (msgp.ByteSize))
+	return
+}
