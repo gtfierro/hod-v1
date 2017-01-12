827f1b4941ad9f99bab80ae28600858111acbfa8
Gabe Fierro <gtfierro225@gmail.com>
2017-01-12 12:19:43 -0800

    plays a little nicer with resizing

--- server/static/js/explore.js
+++ server/static/js/explore.js
@@ -25,8 +25,8 @@ var submit_query = function(query) {
       var width=1000;
       var height=500;
       var svg = d3.select("#mynetwork").append("svg")
-      .attr("width", width)
-      .attr("height", height)
+      .attr("width", "100%")
+      .attr("height", "100%")
       .call(d3.behavior.zoom().on("zoom", function () {
             svg.attr("transform", "translate(" + d3.event.translate + ")" + " scale(" + d3.event.scale + ")")
             }))
