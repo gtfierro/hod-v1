42281973784895586e15b94471d8bd4dfb2ec4af
Gabe Fierro <gtfierro225@gmail.com>
2017-01-11 17:04:55 -0800

    version bump

--- main.go
+++ main.go
@@ -22,7 +22,7 @@ func init() {
 func main() {
 	app := cli.NewApp()
 	app.Name = "hod"
-	app.Version = "0.3.2"
+	app.Version = "0.3.3"
 	app.Usage = "BRICK database and query engine"
 
 	app.Commands = []cli.Command{
