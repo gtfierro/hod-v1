297d9df2b1b8b1956f105b76c1d0caf321f1a87f
Gabe Fierro <gtfierro225@gmail.com>
2017-01-11 16:22:32 -0800

    enable environ var override

--- config/config.go
+++ config/config.go
@@ -69,6 +69,7 @@ func ReadConfig(file string) (*Config, error) {
 	if err := viper.ReadInConfig(); err != nil {
 		return nil, err
 	}
+	viper.AutomaticEnv()
 	c := &Config{
 		DBPath:                 viper.GetString("DBPath"),
 		BrickFrameTTL:          viper.GetString("BrickFrameTTL"),
