8a5715ac2c56f559a310eff524a59c3165bcc8d5
Gabe Fierro <gtfierro225@gmail.com>
2017-01-16 15:19:52 -0800

    remove other extraneous print

--- db/query_test.go
+++ db/query_test.go
@@ -1,7 +1,6 @@
 package db
 
 import (
-	ll "log"
 	"strings"
 	"testing"
 
@@ -32,7 +31,6 @@ func TestQueryPlan(t *testing.T) {
 			"SELECT ?meter WHERE { ?meter rdf:type brick:Power_Meter . ?room rdf:type brick:Room . ?meter bf:isPointOf ?equipment . ?equipment rdf:type ?class . ?class rdfs:subClassOf+ brick:Heating_Ventilation_Air_Conditioning_System . ?zone rdf:type/rdfs:subClassOf* brick:HVAC_Zone . ?equipment bf:feeds+ ?zone . ?zone bf:hasPart ?room . } ;",
 		},
 	} {
-		ll.Println(test.query)
 		q, e := query.Parse(strings.NewReader(test.query))
 		if e != nil {
 			t.Error(e)
