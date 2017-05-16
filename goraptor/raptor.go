084e261089fbfed1ee84d2e4c08401f34fa39032
Gabe Fierro <gtfierro225@gmail.com>
2017-05-16 15:42:54 -0700

    support turtle and ntriples and arbitrary other format parsing

--- goraptor/raptor.go
+++ goraptor/raptor.go
@@ -24,7 +24,7 @@ package turtle
 //   const char *pfx = raptor_namespace_get_counted_prefix(namespace, &pfx_len);
 //   registerNamespace(ns, pfx, ns_len, pfx_len);
 // }
-// void parse_file(char *filename) {
+// void parse_file_turtle(char *filename) {
 //   raptor_world *world = NULL;
 //   raptor_parser* rdf_parser = NULL;
 //   unsigned char *uri_string;
@@ -51,8 +51,74 @@ package turtle
 //
 //   raptor_free_world(world);
 // }
+// void parse_file_ntriples(char *filename) {
+//   raptor_world *world = NULL;
+//   raptor_parser* rdf_parser = NULL;
+//   unsigned char *uri_string;
+//   raptor_uri *uri, *base_uri;
+//
+//   world = raptor_new_world();
+//
+//   rdf_parser = raptor_new_parser(world, "ntriples");
+//
+//   raptor_parser_set_statement_handler(rdf_parser, NULL, handle_triple);
+//   raptor_parser_set_namespace_handler(rdf_parser, NULL, handle_namespace);
+//
+//   uri_string = raptor_uri_filename_to_uri_string(filename);
+//   uri = raptor_new_uri(world, uri_string);
+//   base_uri = raptor_uri_copy(uri);
+//
+//   raptor_parser_parse_file(rdf_parser, uri, base_uri);
+//
+//   raptor_free_parser(rdf_parser);
+//
+//   raptor_free_uri(base_uri);
+//   raptor_free_uri(uri);
+//   raptor_free_memory(uri_string);
+//
+//   raptor_free_world(world);
+// }
+// void parse_file_guess(char *filename) {
+//   raptor_world *world = NULL;
+//   raptor_parser* rdf_parser = NULL;
+//   unsigned char *uri_string;
+//   raptor_uri *uri, *base_uri;
+//
+//   world = raptor_new_world();
+//
+//   rdf_parser = raptor_new_parser(world, "guess");
+//
+//   raptor_parser_set_statement_handler(rdf_parser, NULL, handle_triple);
+//   raptor_parser_set_namespace_handler(rdf_parser, NULL, handle_namespace);
+//
+//   uri_string = raptor_uri_filename_to_uri_string(filename);
+//   uri = raptor_new_uri(world, uri_string);
+//   base_uri = raptor_uri_copy(uri);
+//
+//   raptor_parser_parse_file(rdf_parser, uri, base_uri);
+//
+//   raptor_free_parser(rdf_parser);
+//
+//   raptor_free_uri(base_uri);
+//   raptor_free_uri(uri);
+//   raptor_free_memory(uri_string);
+//
+//   raptor_free_world(world);
+// }
 import "C"
 
+import (
+	"path/filepath"
+)
+
 func (p *Parser) parseFile(filename string) {
-	C.parse_file(C.CString(filename))
+	extension := filepath.Ext(filename)
+	switch extension {
+	case "ttl", "turtle":
+		C.parse_file_turtle(C.CString(filename))
+	case "n3", "ntriples":
+		C.parse_file_ntriples(C.CString(filename))
+	default:
+		C.parse_file_guess(C.CString(filename))
+	}
 }
